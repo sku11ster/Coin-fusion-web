ea46645483a68ac55839b47600b623db6fdc07a5
sku11ster <57558197+sku11ster@users.noreply.github.com>
2023-03-11 21:18:01 +0500

    sign-up-login

--- application.py
+++ application.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
