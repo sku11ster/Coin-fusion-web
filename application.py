ec4accfacaafdecdc56801517239448647fff60b
sku11ster <57558197+sku11ster@users.noreply.github.com>
2023-03-22 18:05:13 +0500

    update (deposit partial)

--- application.py
+++ application.py
@@ -12,5 +12,15 @@ def home():
 def signup():
     return render_template('signup.html')
 
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/login2')
+def login2():
+    return render_template('login2.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
