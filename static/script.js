110608554042643d0b41cea22336ded9c42bb949
sku11ster <57558197+sku11ster@users.noreply.github.com>
2023-03-24 12:46:44 +0500

    update

---
+++ static/script.js
@@ -0,0 +1,84 @@
+//BTC PRICE
+
+fetch("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+  .then(response => response.json())
+  .then(data => {
+    const btcPrice = parseFloat(data.price);
+    const btcPriceSpan = document.getElementById("btc-price");
+    btcPriceSpan.textContent = `$${btcPrice.toFixed(2)}`;
+  })
+  .catch(error => {
+    console.error("Failed to fetch BTC price:", error);
+  });
+
+//BTC PRICE CHANGE
+
+
+fetch("https://api.binance.com/api/v3/ticker/24hr?symbol=BTCUSDT")
+  .then(response => response.json())
+  .then(data => {
+
+    const btcPriceChange = parseFloat(data.priceChangePercent);
+
+    const btcPriceChangeSpan = document.getElementById("btc-growth-rate");
+    btcPriceChangeSpan.textContent = `${btcPriceChange.toFixed(2)}%`;
+  })
+  .catch(error => {
+    console.error("Failed to fetch BTC price change:", error);
+  });
+
+//ETH PRICE
+  
+
+  fetch("https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT")
+    .then(response => response.json())
+    .then(data => {
+     
+      const ethPrice = parseFloat(data.price);
+  
+
+      const ethPriceSpan = document.getElementById("eth-price");
+      ethPriceSpan.textContent = `$${ethPrice.toFixed(2)}`;
+    })
+    .catch(error => {
+      console.error("Failed to fetch ETH price:", error);
+    });
+//ETH GROWTH RATE
+
+fetch("https://api.binance.com/api/v3/ticker/24hr?symbol=ETHUSDT")
+  .then(response => response.json())
+  .then(data => {
+    const ethPriceChange = parseFloat(data.priceChangePercent);
+
+    const ethPriceChangeSpan = document.getElementById("eth-growth-rate");
+    ethPriceChangeSpan.textContent = `${ethPriceChange.toFixed(2)}%`;
+  })
+  .catch(error => {
+    console.error("Failed to fetch ETH price change:", error);
+  });
+//LTC PRICE
+fetch("https://api.binance.com/api/v3/ticker/price?symbol=LTCUSDT")
+  .then(response => response.json())
+  .then(data => {
+    const ltcPrice = parseFloat(data.price);
+    const ltcPriceSpan = document.getElementById("ltc-price");
+    ltcPriceSpan.textContent = `$${ltcPrice.toFixed(2)}`;
+  })
+  .catch(error => {
+    console.error("Failed to fetch LTC price:", error);
+  });
+//LTC PRICE CHANGE
+
+
+
+// Fetch the 24-hour ticker statistics for the LTC/USDT trading pair from the Binance API
+fetch("https://api.binance.com/api/v3/ticker/24hr?symbol=LTCUSDT")
+  .then(response => response.json())
+  .then(data => {
+    const ltcPriceChange = parseFloat(data.priceChangePercent);
+    const ltcPriceChangeSpan = document.getElementById("ltc-growth-rate");
+    ltcPriceChangeSpan.textContent = `${ltcPriceChange.toFixed(2)}%`;
+  })
+  .catch(error => {
+    console.error("Failed to fetch LTC price change:", error);
+  });
