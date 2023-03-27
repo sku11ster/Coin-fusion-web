d18f095e12029030e385a59c8b01fdee736cc100
sku11ster <57558197+sku11ster@users.noreply.github.com>
2023-03-28 01:53:27 +0500

    update qr code

--- application.py
+++ application.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for
 import requests,json
+import qrcode
+import io
+import base64
+
+
 app = Flask(__name__)
 
 
@@ -29,11 +34,27 @@ def home():
 @app.route('/markets')
 def markets():
     return render_template('markets.html')
-    
-@app.route('/deposit-withdrawal')
-def deposit_withdrawal():
-    return render_template('deposit-withdrawal.html')
 
+@app.route('/deposit-withdrawal')
+def deposit_withdrawal_addy():
+    #hamza vro compute the crypto address here and store it in var "addy"
+
+
+
+    qr = qrcode.QRCode(version=1, box_size=10, border=4)
+    qr.add_data("address:" + addy)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    # Convert the image to a base64-encoded string and embed it in the HTML
+    buffered = io.BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    qr_src = f"data:image/png;base64,{img_str}"
+
+    return render_template('deposit-withdrawal.html', address=addy, qr_src=qr_src)
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
